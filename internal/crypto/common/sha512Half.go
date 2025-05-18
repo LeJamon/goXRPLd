78daa5f30d06230cfdc8a43c2335c7d353361b03
Hussenet Thomas <113238884+LeJamon@users.noreply.github.com>
2025-05-18 16:48:24 +0200

    specify better type for return of hash function

--- internal/crypto/common/sha512Half.go
+++ internal/crypto/common/sha512Half.go
@@ -3,7 +3,9 @@ package crypto
 import "crypto/sha512"
 
 // Returns the first 32 bytes of a sha512 hash of a message
-func Sha512Half(msg []byte) []byte {
+func Sha512Half(msg []byte) [32]byte {
 	h := sha512.Sum512(msg)
-	return h[:32]
+	var result [32]byte
+	copy(result[:], h[:32])
+	return result
 }
