21b8df1462beaf476d011bec3bbafe2dee8196f6
Hussenet Thomas <113238884+LeJamon@users.noreply.github.com>
2025-09-12 11:15:04 +0200

    Tempo

--- internal/config/genesis.go
+++ internal/config/genesis.go
@@ -1,1 +1,10 @@
 package config
+import (Ledger)
+
+//should read the
+func getGenesisLedgerFromGenesisFile(c *Config) (genesis.Genesis, error) Ledger{
+
+}
+
+//SHOULD read the json file to get the ammendment array
+func getAdmendmentFromGenesisFile(c *Config) (genesis.Genesis, error) [32]byte {}
--- internal/core/ledger/ledger.go
+++ internal/core/ledger/ledger.go
@@ -1,1 +1,22 @@
 package ledger
+
+import (
+	"github.com/LeJamon/goXRPLd/internal/core/XRPAmount"
+	"github.com/LeJamon/goXRPLd/internal/core/ledger/header"
+	"github.com/LeJamon/goXRPLd/internal/core/shamap"
+)
+
+/**
+TODO
+Rules 	rules_
+**/
+
+type Ledger struct {
+	Immutable bool
+	TxMap     shamap.SHAMap
+	StateMap  shamap.SHAMap
+	Header    header.LedgerHeader
+	Fees      XRPAmount.Fees
+}
+
+//func StoreLedger()
